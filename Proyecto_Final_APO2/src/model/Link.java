4c3f884f12cd4c4370fe8e391fafc72a68cb6a7e
nelson quiñones <nelsonquinones2424@gmail.com>
2018-11-24 13:22:32 -0500

    Agrgado paquete hilos, avances en el procesamiento de hilos

--- Proyecto_Final_APO2/src/model/Link.java
+++ Proyecto_Final_APO2/src/model/Link.java
@@ -1,10 +1,12 @@
 package model;
+import java.net.*;
 
 public class Link extends Palabra{
-
+	
+	private URI identificador;
+	
 	public Link(String palabra, int[] puntuaciones) {
 		super(palabra, puntuaciones);
-		
 	}
 
 }
--- Proyecto_Final_APO2/src/model/Tweet_Aplication.java
+++ Proyecto_Final_APO2/src/model/Tweet_Aplication.java
@@ -1,13 +1,29 @@
 package model;
 
+import java.io.File;
 import java.util.ArrayList;
+
+import Hilos.Hilo_ProcesarDatos;
 
 public class Tweet_Aplication {
 	private Usuario usuarioRaiz;
 	private PalabraRelevante raizRelevante;
-	private ArrayList<Hashtag> tendencias;
-	private ArrayList<Link> tendenciasLinks;
+	private Hashtag tendencias;
+	private Link tendenciasLinks;
 	private Hilo_ProcesarDatos Datos;
+	private ArrayList<String> NombreArchivos;
 	
+	public Tweet_Aplication() {
+		Datos = new Hilo_ProcesarDatos(tendenciasLinks, tendencias,raizRelevante,"Nombre del archivo");
+		Thread a = new Thread(Datos);
+		a.start();
+		while(usuarioRaiz == null) {
+			usuarioRaiz = Datos.getCreado();
+		}
+		System.out.println(usuarioRaiz);
+	}
 	
+	public static void main(String args[]) {
+		Tweet_Aplication lel = new Tweet_Aplication();
+	}
 }
