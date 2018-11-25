9c054cb4f48c2026009ebfadb29206063dab3a84
Marco Antonio Perez Zamora <marcopza99@hotmail.com>
2018-11-25 13:49:00 -0500

    Estructura básica de tendecias

---
+++ Proyecto_Final_APO2/src/controller/TrendsController.java
@@ -0,0 +1,37 @@
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
+import javafx.fxml.Initializable;
+
+public class TrendsController implements Initializable{
+
+    @FXML
+    private ListView<?> hashatagListView;
+
+    @FXML
+    void OrderNWord(ActionEvent event) {
+
+    }
+
+    @FXML
+    void back(MouseEvent event) {
+
+    }
+
+    @FXML
+    void orderDate(ActionEvent event) {
+
+    }
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+	}
+
+}
