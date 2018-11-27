4d02cef7944b5bc383b771ed899cfe219a637b26
Marco Antonio Perez Zamora <marcopza99@hotmail.com>
2018-11-26 19:57:05 -0500

    Documentación de la clase Link

--- Proyecto_Final_APO2/src/model/Link.java
+++ Proyecto_Final_APO2/src/model/Link.java
@@ -17,6 +17,10 @@ public class Link extends Palabra implements Serializable,Agregar{
 		
 	}
 	
+	/**
+	 * agregarUltimoL - Método que agrega un link en el último lugar de la lista enlazada.
+	 * @param temp - El link por agregar.
+	 */
 	public void agregarUltimoL(Link temp) {
 		if(this.getSiguienteLink()== null) {
 			this.setSiguienteLink(temp);
@@ -26,27 +30,53 @@ public class Link extends Palabra implements Serializable,Agregar{
 		
 	}
 
+	/**
+	 * Link - método constructor de la clase. Posteriormente el link lo usa para instanciar un objeto URI.
+	 * @param palabra - Link en cadena de texto como tal de la clase.
+	 * @param puntuaciones - arreglo de puntuaciones que puede tener el link.
+	 * @throws URISyntaxException 
+	 */
 	public Link(String palabra, int[] puntuaciones) throws URISyntaxException {
 		super(palabra, puntuaciones);
 		identificador = new URI(palabra);
 	}
 	
+	/**
+	 * getSiguienteLink - Método que obtiene el siguiente link al que se está usando en el momento de llamar al método.  
+	 * @return Link - el siguiente Link en la lista enlazada.
+	 */
 	public Link getSiguienteLink() {
 		return siguienteLink;
 	}
 
+	/**
+	 * setSiguienteLink - Método que modifica el valor del siguiente link al que se está usando en el momento de llamar al método.
+	 * @param siguienteLink
+	 */
 	public void setSiguienteLink(Link siguienteLink) {
 		this.siguienteLink = siguienteLink;
 	}
-
+	
+	/**
+	 * getIdentificador - Método que obtiene el valor del identificador.
+	 * @return URI - retorna el objeto URI.
+	 */
 	public URI getIdentificador() {
 		return identificador;
 	}
 
+	/**
+	 * setIdentificador - Método que modifica el valor del identificador.
+	 * @param identificador - Nuevo URI que se desea guardar.
+	 */
 	public void setIdentificador(URI identificador) {
 		this.identificador = identificador;
 	}
 	
+	/**
+	 * darLinks - Método que recorre toda la lista enlazada guardando todas las instancias de la clase Link encontradas.
+	 * @param links - ArrayList de tipo objeto en el que se guardarán todas las instancias.
+	 */
 	public void darLinks(ArrayList<Object> links) {
 		if(siguienteLink == null)
 			links.add(this);
@@ -56,6 +86,11 @@ public class Link extends Palabra implements Serializable,Agregar{
 		}
 	}
 	
+	/**
+	 * compareTo - Método que compara dos objetos Link según sus valores lexicográgricos de sus palabras o links en cadena de texto en sí.
+	 * @param nuevo - El link con el cual se quiere comparar la instancia actual.
+	 * @return - Valor negativo, positivo o 0 dependiendo de la diferencia 'actual-nuevo' lexicográficamente. 
+	 */
 	public int compareTo(Link nuevo) {
 		return super.palabra.compareTo(nuevo.palabra);
 	}
